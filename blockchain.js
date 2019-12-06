140b4229542bc0fb1ea947177a992eea6dcb30c1
jdd4494 <jdd4494@users.noreply.github.com>
2019-12-05 22:31:23 -0500

    Add files via upload
    Added javascript calls
    Updated weapon.rell to create weapon nfa and entitees

---
+++ blockchain.js
@@ -0,0 +1,181 @@
+
+const crypto = require('crypto');
+const { util } = require('postchain-client');
+const { 
+  SingleSignatureAuthDescriptor, 
+  DirectoryServiceBase, 
+  ChainConnectionInfo,
+  Blockchain, 
+  FlagsType, 
+  User 
+} = require('ft3-lib');
+ 
+// blockchain connection info
+class DirectoryService extends DirectoryServiceBase {
+  constructor() {
+    super([
+      new ChainConnectionInfo(
+        Buffer.from(
+          '0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF',  
+          'hex'
+        ),
+        'http://localhost:7740'
+      )
+    ]);
+  }
+}
+
+
+/**********************************************************
+@description
+**********************************************************/
+class PlayerInfo {
+  constructor(username, level, strength, speed, skill ) {
+  	this.username = username;
+	this.level = level;
+	this.strength = strength;
+	this.speed = speed;
+	this.skill = skill;
+  }
+
+  serialize() {
+    return [this.username, this.level, this.strength, this.speed, this.skill];
+  }
+}
+
+/**********************************************************
+@description
+**********************************************************/
+class WeaponInfo {
+  constructor(id, name, type, rarity, damage, price){
+  	this.id = id;
+	this.name = name;
+	this.type = type;
+	this.rarity = rarity;
+	this.damage = damage;
+	this.price = price;
+  }
+
+  serialize(){
+    return [this.id, this.name, this.type, this.rarity, this.damage, this.price];
+  }
+}
+
+
+(async () => {
+  // initialize Blockchain object. initialize function receives an id of a blockchain to which 
+  // we want to connect and a directory service which holds blockchain connection info.
+  // Blockchain class provides functions to call rell operations and queries. 
+  // it is used by all ft3 lib entities (Asset, Account, ...) to interact with a blockchain.
+  const blockchain = await Blockchain.initialize(
+      Buffer.from(
+        '0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF', 
+        'hex'
+      ), 
+      new DirectoryService()
+  );
+
+  // create a dapp user. an instance of User object holds data needed to sign transaction
+  // and to interact with the ft3 lib. it doesn't represent dapp account. with a user object
+  // it is possible to access multiple dapp accounts, and a dapp account could have multiple
+  // users linked to it (through authentication descriptor).
+  const keyPair = util.makeKeyPair();
+  const user = new User(
+    keyPair,
+    new SingleSignatureAuthDescriptor(
+      keyPair.pubKey,
+      [FlagsType.Account, FlagsType.Transfer]
+    )
+  );
+
+  // STEP 3 - create a player
+  let playerInfo = new PlayerInfo("player1", 1, 5, 10, 5);
+
+  // call 'my_game.create_player' operation to create a new dapp account. the first parameter is
+  // dapp user. call function uses user keyPair property to sign the transaction, the
+  // rest of parameters are passed to blockchain 'my_game.create_player' operation.
+  try {
+    await blockchain.call(
+      user,
+      'my_game.create_player',
+      playerInfo.serialize(),
+      user.authDescriptor.toGTV()
+    );
+  } catch (error) {
+    console.log(`Error creating new player: ${error.message}`);
+    process.exit(1);
+  }
+	
+  // STEP 4 - lookup user account
+  // get created account details by calling find_player_by_usernamequery
+  const userAccount = await blockchain.query('my_game.find_player_by_username', { 
+  	username: playerInfo.username
+  });
+  console.log(userAccount);
+
+  // STEP 5 - update player
+  playerInfo = new PlayerInfo("player1", 10, 50, 100, 50);
+
+  try{
+    await blockchain.call(
+      user,
+      'my_game.update_player',
+      playerInfo.serialize(),
+      crypto.randomBytes(32)
+    );
+  } catch(error){
+    console.log('Error updating player: ${error.message}');
+    process.exit(1);
+  }
+
+  // get updated account details and print them to console
+  userAccount = await blockchain.query('my_game.find_player_by_username', {
+	username: playerInfo.username
+  });
+  console.log(userAccount);
+  
+  // STEP 7 - create weapon nfa
+  try{
+    await blockchain.call(
+      user,
+      'my_game.create_weapon_nfa'
+    );
+  } catch(error){
+    console.log('Error updating player: ${error.message}');
+    process.exit(1);
+  }
+
+  // STEP 8 - create weapon entitee
+  let id = Math.floor(new Date() / 1000);
+  let weaponInfo = new WeaponInfo(id, "Sword of a 1000 Truths", "sword", "legendary", 1000000, 9999);
+
+  try{
+    await blockchain.call(
+      user,
+      'my_game.create_weapon_entitee',
+      weaponInfo.serialize(),
+      user.authDescriptor.toGTV()
+    );
+  } catch(error){
+    console.log('Error updating player: ${error.message}');
+    process.exit(1);
+  }
+
+  // STEP 9 - search weapons owned by player
+  try{
+    let weapons = await blockchain.query('my_game.find_weapon_by_account_id', {
+      account_id: userAccount.account_id.toString('hex')
+    });
+
+    if(weapons == null || weapons.length == 0){
+      console.log('No weapons owned by player');
+    }else{
+      console.log(weapons);
+    }
+  } catch(error){
+   console.log('Error looking up player weapons: ${error.message}');
+   process.exit(1);
+  }
+  
+
+})()
